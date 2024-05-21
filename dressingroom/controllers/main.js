8a307025444e5aefe9ff15681d2d6a31089a35ba
QVinhDev <vinhnq1103@gmail.com>
2024-05-21 14:45:11 +0700

    update dressing

---
+++ dressingroom/controllers/main.js
@@ -0,0 +1,21 @@
+function changeBikiniTopImage(imageUrl) {
+    document.querySelector('.bikinitop').style.background = `url(${imageUrl})`;
+}
+function changeBikiniBotImage(imageUrl) {
+    document.querySelector('.bikinibottom').style.background = `url(${imageUrl})`;
+}
+function changeShoesImage(imageUrl) {
+    document.querySelector('.feet').style.background = `url(${imageUrl})`;
+}
+function changeHandbagImage(imageUrl) {
+    document.querySelector('.handbag').style.background = `url(${imageUrl})`;
+}
+function changeNecklaceImage(imageUrl) {
+    document.querySelector('.necklace').style.background = `url(${imageUrl})`;
+}
+function changeHairstyleImage(imageUrl) {
+    document.querySelector('.hairstyle').style.background = `url(${imageUrl})`;
+}
+function changeBackgroundImage(imageUrl) {
+    document.querySelector('.background').style.background= `url(${imageUrl})`;
+}
